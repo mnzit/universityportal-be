754cf78e126caba0bec0106a6315a0c7bc9e24bf
[Nawaraj <[nawarajshrestha92@gmail.com]>
2021-12-08 18:59:53 +0545

    Added Subject

---
+++ src/main/java/com/nepalaya/up/model/Subject.java
@@ -0,0 +1,40 @@
+package com.nepalaya.up.model;
+
+import lombok.*;
+
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Date;
+import java.util.Timer;
+
+@Entity
+@Table(name = "SUBJECTS")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject extends BaseEntity<User> {
+
+    @Basic(optional = true)
+    @NotBlank
+    @Size(min = 2, max = 150)
+    @Column(name = "TITLE", nullable = false)
+    private String title;
+
+    @Basic(optional = true)
+    @NotBlank
+    @Size(min = 2, max = 150)
+    @Column(name = "DESCRIPTION", nullable = false)
+    private String description;
+
+    @Basic(optional = true)
+    @NotBlank
+    @Size(min = 2, max = 150)
+    @Column(name = "CREDIT_HOURS", nullable = false)
+    private Long credit_hours;
+}
