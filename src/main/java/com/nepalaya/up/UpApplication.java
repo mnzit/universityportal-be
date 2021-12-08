eb047fad73d6f48342e0974a7c6f9d24628ff111
mnzit <mnzitshakya@gmail.com>
2021-12-08 18:30:31 +0545

    code changes

--- src/main/java/com/nepalaya/up/UpApplication.java
+++ src/main/java/com/nepalaya/up/UpApplication.java
@@ -1,39 +1,13 @@
 package com.nepalaya.up;
 
-import com.nepalaya.up.model.Gender;
-import com.nepalaya.up.model.User;
-import com.nepalaya.up.repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class UpApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UpApplication.class, args);
-	}
-
-
-	@Bean
-	public CommandLineRunner demo(UserRepository userRepository) {
-		return (args) -> {
-			User user = new User();
-
-			user.setFirstName("Anita");
-			user.setLastName("Joshi");
-			user.setAddress("Nepal");
-			user.setGender(Gender.FEMALE);
-			user.setContactNo("980XXXXXXX");
-			user.setEmailAddress("anitajoc@gmail.com");
-			user.setPassword("password1");
-			userRepository.save(user);
-			Thread.sleep(100000);
-			user.setFirstName("Anitas");
-			userRepository.save(user);
-		};
 	}
 
 }
--- src/main/java/com/nepalaya/up/config/AuditingConfig.java
+++ src/main/java/com/nepalaya/up/config/AuditingConfig.java
@@ -1,6 +1,5 @@
 package com.nepalaya.up.config;
 
-import com.nepalaya.up.model.Gender;
 import com.nepalaya.up.model.User;
 import com.nepalaya.up.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
@@ -22,17 +21,6 @@ public class AuditingConfig {
 
     @Bean
     public AuditorAware<User> auditorAware() {
-
-        User user = new User();
-
-        user.setFirstName("Anita");
-        user.setLastName("Joshi");
-        user.setAddress("Nepal");
-        user.setGender(Gender.FEMALE);
-        user.setContactNo("980XXXXXXX");
-        user.setEmailAddress("anitajoc@gmail.com");
-        user.setPassword("password1");
-
-        return () -> Optional.of(user);
+        return () -> Optional.of(userRepository.getById(1L));
     }
 }
