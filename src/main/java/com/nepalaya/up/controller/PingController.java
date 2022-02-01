3fdd7f56fbcffa444d21165013dec0591f7b96f5
mnzit <mnzitshakya@gmail.com>
2022-02-01 23:37:52 +0545

    added requester ip

--- src/main/java/com/nepalaya/up/controller/PingController.java
+++ src/main/java/com/nepalaya/up/controller/PingController.java
@@ -2,6 +2,9 @@ package com.nepalaya.up.controller;
 
 import com.nepalaya.up.constant.ApiConstant;
 import com.nepalaya.up.util.LogUtil;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.Instant;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Controller
 @RequestMapping(ApiConstant.PING)
@@ -19,17 +21,20 @@ public class PingController {
     @Value("${spring.application.name}")
     private String name;
 
-    private Date startedDate;
+    private Instant startedDate;
 
     public PingController() {
-        this.startedDate = new Date();
+        this.startedDate = Instant.now();
     }
 
     @GetMapping
     public String ping(Model model, HttpServletRequest request) {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        DateTimeZone asiaKathmandu = DateTimeZone.forID("Asia/Kathmandu");
+        DateTime now = startedDate.toDateTime(asiaKathmandu);
+
         try {
-            String dateTime = dateFormat.format(startedDate);
+            String dateTime = dateFormat.format(now.toDate());
             model.addAttribute("startedDate", dateTime);
         } catch (Exception e) {
             LogUtil.exception(e);
