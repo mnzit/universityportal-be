58cd4ee754e9fe9d51ab41d4470f7ba938f59d05
mnzit <mnzitshakya@gmail.com>
2022-04-08 20:17:05 +0545

    cors mapping with header exposure

--- src/main/java/com/nepalaya/up/config/CorsConfigurers.java
+++ src/main/java/com/nepalaya/up/config/CorsConfigurers.java
@@ -1,34 +1,28 @@
-package com.nepalaya.up.config;
+
+.package com.nepalaya.up.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfigurers {
 
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        // allow header "Location" to be read by clients to enable them to read the location of an uploaded group logo
-        configuration.addExposedHeader("Location");
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")
+                        .allowCredentials(true)
+                        .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+            }
+        };
     }
+
 
 }
