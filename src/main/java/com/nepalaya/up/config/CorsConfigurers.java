c48dab84608226d3f7c82988f031051026a3b3ac
Manjit Shakya <mnzitshakya@gmail.com>
2022-08-03 21:20:28 +0545

    Update CorsConfigurers.java

--- src/main/java/com/nepalaya/up/config/CorsConfigurers.java
+++ src/main/java/com/nepalaya/up/config/CorsConfigurers.java
@@ -16,7 +16,7 @@ public class CorsConfigurers {
             public void addCorsMappings(CorsRegistry registry) {
                 registry
                         .addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500/", "https://mnzit.github.io","http://localhost:4200","https://universityportal-fe.pages.dev")
+                        .allowedOrigins("http://localhost:9081/","http://127.0.0.1:5500/", "https://mnzit.github.io","http://localhost:4200","https://universityportal-fe.pages.dev")
                         .allowCredentials(true)
                         .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
                         .allowedMethods("GET","POST","PUT","DELETE")
